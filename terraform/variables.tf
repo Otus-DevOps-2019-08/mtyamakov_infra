4b6e3d5abafe545fd7d3d3261af6d4644750169f
m.tyamakov <m.tyamakov@bi.zone>
2020-01-09 11:39:57 +0300

    Added terraform files

---
+++ terraform/variables.tf
@@ -0,0 +1,22 @@
+variable project {
+  description = "Project ID"
+}
+variable region {
+  description = "Region"
+  # Значение по умолчанию
+  default = "europe-west1"
+}
+variable public_key_path {
+  # Описание переменной
+  description = "Path to the public key used for ssh access"
+}
+variable disk_image {
+  description = "Disk image"
+}
+variable private_key_path {
+  description = "Path to the private key userd for ssh access"
+}
+variable zone {
+  description = "Zone"
+  default     = "europe-west1-b"
+}
